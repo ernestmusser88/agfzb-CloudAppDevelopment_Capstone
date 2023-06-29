8064fbf1f9b5dd8740f43c90a569ba1a714f03b0
ernestmusser88 <ernestmusseriii@gmail.com>
2023-06-28 21:18:37 -0400

    added get reviews functions

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -46,6 +46,49 @@ class CarModel(models.Model):
                 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class CarReview:
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, sentiment, id):
+        # Dealer dealership
+        self.dealership = dealership
+        # Dealer name
+        self.name = name
+        # Dealer purchase
+        self.purchase = purchase
+        # Dealer review
+        self.review = review
+        # Location purchase_date
+        self.purchase_date = purchase_date
+        # Location car_make
+        self.car_make = car_make
+        # Dealer car_model
+        self.car_model = car_model
+        # Dealer sentiment
+        self.sentiment = sentiment
+        # Dealer id
+        self.id = id
+    def __str__(self):
+        return "Dealer name: " + self.name
